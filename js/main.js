557299fc52aeb5369958d5ceb1966d2295383f4f
Radulescu Alexandru <radulescu.andu200@gmail.com>
2021-09-18 19:32:38 +0300

    commitforth-commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const meniu = document.querySelector('.meniu');
 const span = meniu.querySelectorAll('span');
 const ul_meniu = document.querySelector('nav ul');
@@ -21,6 +22,9 @@ meniu.addEventListener('click', (e) => {
         
     }
 });
+const description=document.querySelector('.description');
+const title_about=document.querySelector('.title-about');
+const personal_interests=document.querySelector('.personal-interests');
 
 const title_projects=document.querySelector('.title-project');
 const profile=document.querySelector('.profile');
@@ -28,144 +32,12 @@ window.addEventListener('load',function(){
     profile.style.transform="scale(1)";
     profile.style.opacity="1";
 });
-const description=document.querySelector('.description');
-const title_about=document.querySelector('.title-about');
-const personal_interests=document.querySelector('.personal-interests');
+
 const title_footer=document.querySelector('.title-footer');
 const title_skills=document.querySelector('.title-skills');
 const title_resume=document.querySelector('.title-resume');
 
 
-window.addEventListener('scroll',function(){
-    if (window.matchMedia("(max-width: 720px)").matches) {
-       if(window.scrollY>800 &&window.scrollY <900){
-        title_about.style.transform="translateY(0px)";
-        title_about.style.opacity="1";
-        description.style.transform="translateX(0px)";
-        personal_interests.style.transform="scale(1)";
-        personal_interests.style.opacity="1";
-
-        }else if(window.scrollY>1900&&window.scrollY<1950){
-            title_skills.style.transform="translateY(0px)";
-            title_skills.style.opacity="1";
-        }else if(window.scrollY>2400&&this.window.scrollY<2500){
-            optiunewidth();
-            counterF(12);
-            animatie(5);
-        }else if(window.scrollY>3100 &&window.scrollY<3150){
-            title_resume.style.transform="translateY(0px)";
-            title_resume.style.opacity="1";
-        }else if(window.scrollY>3150 &&window.scrollY<3200){
-            animatie_resume();
-        }else if(window.scrollY>5100&&window.scrollY<5150){
-            title_projects.style.transform="translateY(0px)";
-            title_projects.style.opacity="1";
-        }
-        else if(window.scrollY>6350 && window.scrollY<6400){
-            title_footer.style.transform="translateY(0px)";
-            title_footer.style.opacity="1";
-        }
-    }else if(window.matchMedia("(max-width: 1200px)").matches){
-        if(window.scrollY>900 &&window.scrollY<1000){
-            title_about.style.transform="translateY(0px)";
-            title_about.style.opacity="1";
-            description.style.transform="translateX(0px)";
-            personal_interests.style.transform="scale(1)";
-            personal_interests.style.opacity="1";
-        }else if(window.scrollY>2000&&window.scrollY<2150){ 
-            title_skills.style.transform="translateY(0px)";
-            title_skills.style.opacity="1";
-        }
-            else if(window.scrollY>2150&&window.scrollY<2250){
-                optiunewidth();
-                counterF(18);   
-            animatie(5);
-            }else if(window.scrollY>2600 && window.scrollY<2700){
-                title_resume.style.transform="translateY(0px)";
-                title_resume.style.opacity="1";;
-            }else if(window.scrollY>2800 && window.scrollY<2850){
-                animatie_resume();
-            }else if(window.scrollY>3700 && window.scrollY<3800){
-                title_projects.style.transform="translateY(0px)";
-            title_projects.style.opacity="1"
-
-
-            }else if(window.scrollY>5700 && window.scrollY<5750){
-                title_footer.style.transform="translateY(0px)";
-                title_footer.style.opacity="1";
-
-        
-
-            }
-            
-    }else if(window.matchMedia("(max-width: 1800px)").matches){
-        if(window.scrollY>900 && window.scrollY<1000){
-            title_about.style.transform="translateY(0px)";
-            title_about.style.opacity="1";
-            description.style.transform="translateX(0px)";
-            personal_interests.style.transform="scale(1)";
-            personal_interests.style.opacity="1";
-        }else if(window.scrollY>1750 &&window.scrollY<1800){
-            title_skills.style.transform="translateY(0px)";
-            title_skills.style.opacity="1";
-        }
-        
-        else if(window.scrollY>2000 && window.scrollY<2200){
-            optiunewidth();
-            counterF(18);
-            animatie(5);
-        }
-        else if(window.scrollY>2600 && window.scrollY<2650){
-            title_resume.style.transform="translateY(0px)";
-            title_resume.style.opacity="1";
-        }
-        else if(window.scrollY>2800 &&window.scrollY<2900){
-            animatie_resume();
-        }else if(window.scrollY>3300 && window.scrollY<3400){
-            title_projects.style.transform="translateY(0px)";
-            title_projects.style.opacity="1"
-        }
-        else if(window.scrollY>4500 && window.scrollY<4550){
-            title_footer.style.transform="translateY(0px)";
-            title_footer.style.opacity="1";
-        }
-    }
-    
-    else if(window.matchMedia("(min-width: 1800px)").matches){
-        if(window.scrollY>800&&window.scrollY<850){
-            title_about.style.transform="translateY(0px)";
-            title_about.style.opacity="1";
-            description.style.transform="translateX(0px)";
-            personal_interests.style.transform="scale(1)";
-            personal_interests.style.opacity="1";
-        }else if(window.scrollY>1500 && this.window.scrollY<1600){
-            title_skills.style.transform="translateY(0px)";
-            title_skills.style.opacity="1";
-        }else if(window.scrollY>1800 && window.scrollY<1900){
-            optiunewidth();
-            counterF(25);
-            animatie(5);
-        }else if(window.scrollY>2200 && window.scrollY<2300){
-            title_resume.style.transform="translateY(0px)";
-            title_resume.style.opacity="1";
-        }else if(window.scrollY>2800 && window.scrollY<2850){
-            animatie_resume();
-        }else if(window.scrollY>3700 && window.scrollY<3800){
-            title_projects.style.transform="translateY(0px)";
-            title_projects.style.opacity="1"
-        }else if(window.scrollY>4350 && window.scrollY<4400){
-            title_footer.style.transform="translateY(0px)";
-            title_footer.style.opacity="1";
-
-        }
-    }
-})
-
-const about=document.querySelector('.about');
-console.log(about);
-about.addEventListener('scroll',function(){
-    alert('a');
-})
 
 //animation counting
 const counters = document.querySelectorAll('.counter');
@@ -220,8 +92,69 @@ function counterF(timp) {
 
 const element_resume=document.querySelectorAll('.element_educ_exp');
 
+const about=document.querySelector('.about');
+const skill=document.querySelector('.skills');
+const resume=document.querySelector('.resume');
+const project=document.querySelector('.projects');
+const contact=document.querySelector('.contact');
+window.addEventListener('scroll',function(){
+    if(window.scrollY>=about.offsetTop-180&& window.scrollY<=about.offsetTop){
+        about_animation();
+        title_animation(about);
+    }else if(window.scrollY>=skill.offsetTop-180&&window.scrollY<=skill.offsetTop){
+       skill_animation();
+       title_animation(skill);
+       optiunewidth();
+       counterF(12);
+       animatie(5);
+    }else if(window.scrollY>=resume.offsetTop-180&&window.scrollY<=resume.offsetTop){
+        animation_resume();
+        title_animation(resume);
+        animatie_resume_elemente();
+    }else if(window.scrollY>=project.offsetTop-180&&window.scrollY<=project.offsetTop){
+        
+        animatie_project();
+        title_animation(project);
 
-function animatie_resume(){
+    }else if(window.scrollY>=contact.offsetTop-180&&window.scrollY<=contact.offsetTop){
+contact_animation();
+title_animation(contact);
+    }
+})
+
+
+function contact_animation(){
+    title_footer.style.transform="translateY(0px)";
+    title_footer.style.opacity="1";
+}
+function about_animation(){
+    title_about.style.transform="translateY(0px)";
+    title_about.style.opacity="1";
+    description.style.transform="translateX(0px)";
+    personal_interests.style.transform="scale(1)";
+    personal_interests.style.opacity="1";
+}
+function animatie_project(){
+    title_projects.style.transform="translateY(0px)";
+        title_projects.style.opacity="1"
+}
+function animation_resume(){
+    title_resume.style.transform="translateY(0px)";
+    title_resume.style.opacity="1";
+}
+function skill_animation(){
+    title_skills.style.transform="translateY(0px)";
+    title_skills.style.opacity="1";
+}
+function title_animation(element){
+    const title_sectiune=element.querySelector('.title-sectiune');
+    title_sectiune.style.animation ="bounce 2s 0.5s both";
+}
+
+
+
+
+function animatie_resume_elemente(){
     element_resume.forEach(element=>{
         element.style.transform="translateY(0px)";
         element.style.opacity="1";
